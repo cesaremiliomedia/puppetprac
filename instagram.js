effa029551355470048748072c4602a9147d183b
Cesar E. Castro <you@example.com>
2021-04-01 19:53:47 -0500

    checkout

---
+++ instagram.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+ const browser = await puppeteer.launch({ headless: false });
+ const page = await browser.newPage();
+ await page.goto("https://www.storeavenuedesarts.com/");
+ let click = await page.$x(
+  '//*[@id="shopify-section-featured-collections"]/div/div[2]/div[2]/div/a/div[1]'
+ );
+ await click[0].click();
+ await page.waitForTimeout(1000);
+ let click2 = await page.$x('//*[@id="AddToCart-product-template"]');
+ await click2[0].click();
+ await page.waitForTimeout(1000);
+ let click3 = await page.$x(
+  "/html/body/div[3]/main/div/div/form/footer/div/div/input[2]"
+ );
+ await click3[0].click();
+})();
